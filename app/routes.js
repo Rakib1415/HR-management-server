8496bb79d4e33504fb69cf8e1a461ab7bcff2d7a
Rakib1415 <rakibiuaece@gmail.com>
2023-02-09 03:06:51 +0600

    add port

--- app/routes.js
+++ app/routes.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 
 router.use(require('../routes'));
 
-// router.get('/health', (_req, res) => {
-//   res.status(200).json({ message: 'success' });
-// });
+router.get('/health', (_req, res) => {
+    res.status(200).json({ message: 'success' });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const app = require('./app/app');
 const db = require('./models');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // database connection
 db.sequelize
