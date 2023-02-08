b539c601ebdf7fb71196742d587e4ebd959cbdf7
Rakib1415 <rakibiuaece@gmail.com>
2023-02-09 02:50:57 +0600

    fixs: a bug fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const app = require('./app/app');
 const db = require('./models');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 // database connection
 db.sequelize
